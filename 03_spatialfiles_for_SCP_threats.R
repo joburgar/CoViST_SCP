e6bf6192d8195cc399e59e6b0f6c351fd7dc3d7e
Burgar <Joanna.Burgar@gov.bc.ca>
2021-06-24 16:02:00 -0700

    Exploring threat list data

--- 03_spatialfiles_for_SCP_threats.R
+++ 03_spatialfiles_for_SCP_threats.R
@@ -16,8 +16,7 @@
 # tz = Sys.timezone() # specify timezone in BC
 
 # Load Packages
-# list.of.packages <- c("tidyverse", "lubridate","chron","bcdata", "bcmaps","sf", "rgdal", "readxl", "Cairo", "rjags","coda",
-#                       "OpenStreetMap", "ggmap", "SightabilityModel","truncnorm", "doParallel", "nimble", "scrbook", "xtable")
+list.of.packages <- c("tidyverse", "lubridate","chron","bcdata", "bcmaps","sf", "rgdal", "readxl", "Cairo", "OpenStreetMap", "ggmap")
 # Check you have them and load them
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -26,5 +25,20 @@ lapply(list.of.packages, require, character.only = TRUE)
 
 GISDir <- "//spatialfiles.bcgov/work/wlap/sry/Workarea/jburgar/CoViST"
 
+# load relevant spatial files
 aoi <- st_read(dsn=GISDir, layer="CoViST_Study_Boundary_June2021") # updated to reflect decision at June 22 STSA meeting
-aoi.CH.EO.250m <- st_read(dsn=GISDir, layer="aoi.CH.EO.250m")
+aoi.CH.EO.250m <- st_read(dsn=paste(getwd(),"/out", sep=""), layer="aoi.CH.EO.250m")
+FA_SpRich <- st_read(dsn=paste(getwd(),"/out", sep=""), layer="Focal_Area")
+
+
+# load attribute table
+threats <- read_excel("data/threats_list_May-06-21.xlsx",sheet = 2, trim_ws = TRUE, col_types = c("text")) %>% type.convert()
+threats %>% group_by(Species_Common, Severity, Impact) %>% count(threat_desc, sub_threat_desc)
+threats %>% count(ELCODE)
+nrow(threats)
+nrow(threats %>% filter(!is.na(sub_threat_desc)))
+threats_sub <- threats %>% filter(!is.na(sub_threat_desc))
+threats_sub %>% count(Impact)
+threats_sub %>% group_by(Species_Common, Severity, Impact) %>% count(threat_desc)
+as.data.frame(threats_sub %>% filter(Impact!="Low") %>% group_by(Species_Common) %>% count(threat_desc))
+
